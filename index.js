0d96c8b5c36a2147566ae1678fd4da08b4c88719
Ravi <kkiran.ror@gmail.com>
2020-03-13 04:59:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,13 @@
       if (line.length == 0){
         return "The line is currently empty.";
       } else {
+        for (let i = 0; i < line.length; i++){
+          let idx = i + 1;
+          list += `${idx}. line[i]`
+          if (idx != line.length){
+            list += ', '
+          }
+        }
 
           return `The line is currently: 1. Bill, 2. Jane, 3. Ann`
       }
