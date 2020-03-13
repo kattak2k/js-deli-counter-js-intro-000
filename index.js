30e2a1b1d7e20f79aec8bea8565c1eb12a5fe3fc
Ravi <kkiran.ror@gmail.com>
2020-03-13 04:44:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@
       idx = katzDeli.indexOf(x) + 1
      return  `Welcome, ${x}. You are number ${idx} in line.`
     }
+
+    function nowServing(katzDeli, x) {
+      let idx = 0;
+      katzDeli.push(x);
+      idx = katzDeli.indexOf(x) + 1
+     return  `Welcome, ${x}. You are number ${idx} in line.`
+    }
