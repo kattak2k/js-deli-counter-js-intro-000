e4ffb41d3c264fd05e8eb8b2f30148f484ac153b
Ravi <kkiran.ror@gmail.com>
2020-03-13 04:52:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@
 
     function currentLine(line) {
        var person
-      if (deli.length == 0){
-        return "There is nobody waiting to be served!";
+      if (line.length == 0){
+        return "The line is currently empty.";
       } else {
         person = deli.shift();
         return `Currently serving ${person}.`
