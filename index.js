b74dcd6d0f37fbc2a568c90f3c4bbd33754879fb
Ravi <kkiran.ror@gmail.com>
2020-03-13 04:44:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
     let katzDeli = [];
 
     function takeANumber(katzDeli, x) {
-      let idx;
+      let idx = 0;
       katzDeli.push(x);
       idx = katzDeli.indexOf(x) + 1
      return  `Welcome, ${x}. You are number ${idx} in line.`
